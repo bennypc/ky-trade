30c405d5c6e68c893d3f969a01bafbf9ddb6ace3
bennypc <benny.pincha@gmail.com>
2023-02-09 15:45:55 -0800

    blank

--- src/App.js
+++ src/App.js
@@ -34,15 +34,9 @@ class LambdaDemo extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
+      <div>
+        
+      </div>>
     )
   }
 }
