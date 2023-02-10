2af16380a61d1874818952d7349ab42d86b241a7
bennypc <benny.pincha@gmail.com>
2023-02-09 16:26:03 -0800

    animated

--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import TextTransition, { presets } from 'react-text-transition';
 
 const Timer = () => {
   const [seconds, setSeconds] = useState(0);
@@ -8,7 +9,7 @@ const Timer = () => {
     const startTime = new Date(2023, 1, 2); // February 2, 2023
     const interval = setInterval(() => {
       const now = new Date();
-      setSeconds((now - startTime) / 10000);
+      setSeconds((now - startTime) / 1000);
       setMilliseconds((now - startTime) / 11);
     }, 1000);
 
@@ -17,12 +18,27 @@ const Timer = () => {
 
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      <h1 className="font-bold text-3xl mt-16 mx-4 text-center">
-        {Math.floor(seconds)} questions asked to ky
-      </h1>
-      <h1 className="font-bold text-3xl mt-16 mx-4 text-center">
-        {Math.floor(milliseconds)} dicks ridden by messy
-      </h1>
+      <div className="inline">
+        <h1 className="inline font-bold text-xl lg:text-4xl mt-16 float-left	">
+          <TextTransition springConfig={presets.wobbly}>
+            {Math.floor(seconds)}
+          </TextTransition>
+        </h1>
+        <h1 className="inline font-bold text-xl lg:text-4xl mt-16  text-center float-right	">
+          &nbsp; questions asked to ky
+        </h1>
+      </div>
+
+      <div className="inline">
+        <h1 className="inline font-bold text-xl lg:text-4xl mt-16 float-left	">
+          <TextTransition springConfig={presets.wobbly}>
+            {Math.floor(milliseconds)}
+          </TextTransition>
+        </h1>
+        <h1 className="inline font-bold text-xl lg:text-4xl mt-16  text-center float-right	">
+          &nbsp; dicks ridden by messy
+        </h1>
+      </div>
     </div>
   );
 };
